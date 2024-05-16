07de86334920b2e956172aefd3716392b4819999
Imrul Anwar <imrulanwar836@gmail.com>
2024-05-16 10:10:56 +0600

    added how to provide repository and useCases

--- app/src/main/java/com/imrul/replog/di/AppModule.kt
+++ app/src/main/java/com/imrul/replog/di/AppModule.kt
@@ -3,7 +3,21 @@ package com.imrul.replog.di
 import android.content.Context
 import androidx.room.Room
 import com.imrul.replog.core.Constants.DATABASE_NAME
+import com.imrul.replog.feature_workout.data.data_source.WorkoutDao
 import com.imrul.replog.feature_workout.data.data_source.WorkoutDatabase
+import com.imrul.replog.feature_workout.data.repository.WorkoutRepositoryImp
+import com.imrul.replog.feature_workout.domain.repository.WorkoutRepository
+import com.imrul.replog.feature_workout.domain.use_cases.DeleteExercise
+import com.imrul.replog.feature_workout.domain.use_cases.DeleteSet
+import com.imrul.replog.feature_workout.domain.use_cases.DeleteWorkout
+import com.imrul.replog.feature_workout.domain.use_cases.GetAllExercisesByWorkoutId
+import com.imrul.replog.feature_workout.domain.use_cases.GetAllSetsByExerciseId
+import com.imrul.replog.feature_workout.domain.use_cases.GetAllWorkouts
+import com.imrul.replog.feature_workout.domain.use_cases.GetWorkoutById
+import com.imrul.replog.feature_workout.domain.use_cases.InsertExercise
+import com.imrul.replog.feature_workout.domain.use_cases.InsertSet
+import com.imrul.replog.feature_workout.domain.use_cases.InsertWorkout
+import com.imrul.replog.feature_workout.domain.use_cases.WorkoutUseCases
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -25,4 +39,23 @@ object AppModule {
     fun workoutDao(
         database: WorkoutDatabase
     ) = database.workoutDao()
+
+    @Singleton
+    @Provides
+    fun provideWorkoutRepository(dao: WorkoutDao) = WorkoutRepositoryImp(dao)
+
+    @Singleton
+    @Provides
+    fun provideWokroutUseCases(repository: WorkoutRepository) = WorkoutUseCases(
+        deleteExercise = DeleteExercise(repository),
+        deleteSet = DeleteSet(repository),
+        deleteWorkout = DeleteWorkout(repository),
+        getAllExercisesByWorkoutId = GetAllExercisesByWorkoutId(repository),
+        getAllSetsByExerciseId = GetAllSetsByExerciseId(repository),
+        getAllWorkouts = GetAllWorkouts(repository),
+        getWorkoutById = GetWorkoutById(repository),
+        insertExercise = InsertExercise(repository),
+        insertSet = InsertSet(repository),
+        insertWorkout = InsertWorkout(repository)
+    )
 }
---
+++ app/src/main/java/com/imrul/replog/feature_workout/domain/use_cases/WorkoutUseCases.kt
@@ -0,0 +1,14 @@
+package com.imrul.replog.feature_workout.domain.use_cases
+
+data class WorkoutUseCases(
+    val deleteExercise: DeleteExercise,
+    val deleteSet: DeleteSet,
+    val deleteWorkout: DeleteWorkout,
+    val getAllExercisesByWorkoutId: GetAllExercisesByWorkoutId,
+    val getAllSetsByExerciseId: GetAllSetsByExerciseId,
+    val getAllWorkouts: GetAllWorkouts,
+    val getWorkoutById: GetWorkoutById,
+    val insertExercise: InsertExercise,
+    val insertSet: InsertSet,
+    val insertWorkout: InsertWorkout
+)
