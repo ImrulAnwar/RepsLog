6755548059746d5a871f50597caac2604f99ce24
Imrul Anwar <imrulanwar836@gmail.com>
2024-10-27 13:24:53 +0600

    insert and delete function written

--- app/src/main/java/com/imrul/replog/feature_workout/data/repository/WorkoutDatasourceImpl.kt
+++ app/src/main/java/com/imrul/replog/feature_workout/data/repository/WorkoutDatasourceImpl.kt
@@ -2,6 +2,11 @@ package com.imrul.replog.feature_workout.data.repository
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.imrul.replog.core.Constants.EXERCISES_COLLECTION
+import com.imrul.replog.core.Constants.MEASUREMENTS_COLLECTION
+import com.imrul.replog.core.Constants.NOTES_COLLECTION
+import com.imrul.replog.core.Constants.SESSIONS_COLLECTION
+import com.imrul.replog.core.Constants.SETS_COLLECTION
 import com.imrul.replog.core.Constants.WORKOUTS_COLLECTION
 import com.imrul.replog.feature_workout.domain.model.Exercise
 import com.imrul.replog.feature_workout.domain.model.Note
@@ -41,39 +46,139 @@ class WorkoutDatasourceImpl(
     }
 
     override suspend fun insertExercise(exercise: Exercise): String {
-        TODO("Not yet implemented")
+        val documentId = exercise.id ?: fireStore.collection(EXERCISES_COLLECTION).document().id
+        auth.currentUser?.uid.let { userId ->
+            val data = hashMapOf(
+                "id" to documentId,
+                "name" to exercise.name,
+                "imageUrl" to exercise.imageUrl,
+                "targetMuscleGroup" to exercise.targetMuscleGroup,
+                "weightType" to exercise.weightType,
+                "userId" to userId
+            )
+            fireStore
+                .collection(EXERCISES_COLLECTION)
+                .document(documentId)
+                .set(data)
+                .await()
+        }
+        return documentId
     }
 
     override suspend fun insertSet(set: Set): String {
-        TODO("Not yet implemented")
+        val documentId = set.id ?: fireStore.collection(SETS_COLLECTION).document().id
+        auth.currentUser?.uid.let { userId ->
+            val data = hashMapOf(
+                "id" to documentId,
+                "setType" to set.setType,
+                "weightValue" to set.weightValue,
+                "reps" to set.reps,
+                "isDone" to set.isDone,
+                "sessionIdForeign" to set.sessionIdForeign,
+                "timerDuration" to set.timerDuration,
+                "userId" to userId
+            )
+            fireStore
+                .collection(SETS_COLLECTION)
+                .document(documentId)
+                .set(data)
+                .await()
+        }
+        return documentId
     }
 
     override suspend fun insertSession(session: Session): String {
-        TODO("Not yet implemented")
+        val documentId = session.id ?: fireStore.collection(SESSIONS_COLLECTION).document().id
+        auth.currentUser?.uid.let { userId ->
+            val data = hashMapOf(
+                "id" to documentId,
+                "exerciseIdForeign" to session.exerciseIdForeign,
+                "exerciseName" to session.exerciseName,
+                "workoutIdForeign" to session.workoutIdForeign,
+                "repsSlower" to session.repsSlower,
+                "weightUnit" to session.weightUnit,
+                "setCount" to session.setCount,
+                "isTimerEnabled" to session.isTimerEnabled,
+                "bestSet" to session.bestSet,
+                "userId" to userId
+            )
+            fireStore
+                .collection(SESSIONS_COLLECTION)
+                .document(documentId)
+                .set(data)
+                .await()
+        }
+        return documentId
     }
 
     override suspend fun insertNote(note: Note): String {
-        TODO("Not yet implemented")
+        val documentId = note.id ?: fireStore.collection(NOTES_COLLECTION).document().id
+        auth.currentUser?.uid.let { userId ->
+            val data = hashMapOf(
+                "id" to documentId,
+                "idForeign" to note.idForeign,
+                "belongsTo" to note.belongsTo,
+                "isPinned" to note.isPinned,
+                "content" to note.content,
+                "userId" to userId
+            )
+            fireStore
+                .collection(NOTES_COLLECTION)
+                .document(documentId)
+                .set(data)
+                .await()
+        }
+        return documentId
     }
 
     override suspend fun deleteWorkout(workout: Workout) {
-        TODO("Not yet implemented")
+        workout.id?.let { documentId ->  // Ensure there's an ID to delete
+            fireStore
+                .collection(WORKOUTS_COLLECTION)
+                .document(documentId)
+                .delete()
+                .await()
+        }
     }
 
     override suspend fun deleteExercise(exercise: Exercise) {
-        TODO("Not yet implemented")
+        exercise.id?.let { documentId ->  // Ensure there's an ID to delete
+            fireStore
+                .collection(EXERCISES_COLLECTION)
+                .document(documentId)
+                .delete()
+                .await()
+        }
     }
 
     override suspend fun deleteSet(set: Set) {
-        TODO("Not yet implemented")
+        set.id?.let { documentId ->  // Ensure there's an ID to delete
+            fireStore
+                .collection(SETS_COLLECTION)
+                .document(documentId)
+                .delete()
+                .await()
+        }
     }
 
     override suspend fun deleteSession(session: Session) {
-        TODO("Not yet implemented")
+        session.id?.let { documentId ->  // Ensure there's an ID to delete
+            fireStore
+                .collection(SESSIONS_COLLECTION)
+                .document(documentId)
+                .delete()
+                .await()
+        }
     }
 
     override suspend fun deleteNote(note: Note) {
-        TODO("Not yet implemented")
+        note.id?.let { documentId ->  // Ensure there's an ID to delete
+            fireStore
+                .collection(NOTES_COLLECTION)
+                .document(documentId)
+                .delete()
+                .await()
+        }
     }
 
     override suspend fun getAllWorkouts(): Flow<List<Workout>> = callbackFlow {
@@ -89,7 +194,9 @@ class WorkoutDatasourceImpl(
                 }
 
                 // Map each document to a Measurement object and emit the list
-                val workouts = querySnapshot?.documents?.mapNotNull { it.toObject(Workout::class.java) } ?: emptyList()
+                val workouts =
+                    querySnapshot?.documents?.mapNotNull { it.toObject(Workout::class.java) }
+                        ?: emptyList()
                 trySend(workouts)  // Emit the list of measurements
             }
 
